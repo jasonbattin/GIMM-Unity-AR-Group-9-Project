1144583d1b6b7ba324d7188db76ba377b216d87a
ChrisPedraza07 <151283012+ChrisPedraza07@users.noreply.github.com>
2023-12-11 21:20:07 -0700

    Added opening scene with IntroVid
    Created new scene that automatically plays intro vid and when finished it loads the AR scan scene

---
+++ Assets/Video/PlayVideo.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(VideoPlayer))]
+[RequireComponent(typeof(AudioSource))]
+public class PlayVideo : MonoBehaviour
+{
+    public VideoClip videoClip;
+    public string nextSceneName; // Name of the next scene to load
+
+    private VideoPlayer videoPlayer;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        audioSource = GetComponent<AudioSource>();
+
+        videoPlayer.playOnAwake = false;
+        audioSource.playOnAwake = false;
+
+        videoPlayer.source = VideoSource.VideoClip;
+        videoPlayer.clip = videoClip;
+
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+
+        videoPlayer.renderMode = VideoRenderMode.MaterialOverride;
+        videoPlayer.targetMaterialRenderer = GetComponent<Renderer>();
+        videoPlayer.targetMaterialProperty = "_MainTex";
+
+        videoPlayer.loopPointReached += EndReached; // Subscribe to the loopPointReached event
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+            PlayPause();
+    }
+
+    private void PlayPause()
+    {
+        if (videoPlayer.isPlaying)
+            videoPlayer.Pause();
+        else
+            videoPlayer.Play();
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(2); // Load the next scene
+    }
+}
