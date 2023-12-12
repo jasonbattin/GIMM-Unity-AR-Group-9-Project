b88b63efcddfb59ba2677ee04328de34bc2847f8
jasonbattin <144022437+jasonbattin@users.noreply.github.com>
2023-12-12 15:59:44 -0700

    Adjusted unused code and other

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -17,17 +17,9 @@ public class MainMenu : MonoBehaviour
     public void PlayGame(int scene)
     {
         LoadScene(scene);
-        /*Debug.Log("PlayGame method called");
-        videoPlayer.loopPointReached += EndReached; // Subscribe to the event
-        videoPlayer.Play();*/
+
     }
 
-    /*private void EndReached(VideoPlayer vp)
-    {
-        Debug.Log("Video finished playing.");
-        vp.loopPointReached -= EndReached; // Unsubscribe to avoid this method being called again
-        LoadNextScene();
-    }*/
     void LoadScene(int scene)
     {
         //todo add logic deciding which scene to load based off AR image target
@@ -40,11 +32,6 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Loading main menu...");
         SceneManager.LoadScene("MainMenu");
     }
-    //logic for replaying level
-    /*public void ReplayLevel()
-    {
-        SceneManager.LoadScene("example Level 1");
-    }*/
 
     public void QuitGame()
     {
